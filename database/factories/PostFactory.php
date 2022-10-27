c7c89e087fb85cb2680f0763fc18b2ce5a4ea169
MTkenko <tomoaki.miyatake@gmail.com>
2022-10-28 02:40:03 +0900

    Add:Seeding_file

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+use Faker\Generator as Faker;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'user_id' => 1,
+            'title' => fake()->word,
+            'date' => date('Y_m_d'),
+            'body' => fake()->text($maxNbChars = 6),
+            'rating_1' => 1,
+            'rating_2' => 1,
+            //'created_at' => $faker->unixTime(),
+        ];
+    }
+}
